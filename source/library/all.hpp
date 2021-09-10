e2d4f5df0c5993a52dea0a0a7f823ea7e85df4a5
yahya mohammed <goldroger300@gmail.com>
2021-09-10 20:40:38 +0300

    include all headers into one

---
+++ source/library/all.hpp
@@ -0,0 +1,11 @@
+
+#pragma once
+#ifndef ALL_HPP
+#define ALL_HPP
+
+#include "core/numeric_methods.hpp"
+#include "core/constants.hpp"
+#include "core/matrix.hpp"
+#include "core/numeric.hpp"
+
+#endif // ALL_HPP
