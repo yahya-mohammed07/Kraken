416be40f40562c8ff184417bbd7abc538db89d6d
yahya-mohammed07 <goldroger300@gmail.com>
2021-09-11 09:22:30 +0300

    `numeric_methods` tests

--- tests/numeric_methods_tests.cpp
+++ tests/numeric_methods_tests.cpp
@@ -6,18 +6,18 @@
 TEST_CASE("CHECK GAUSS-ELIMINATION AND DETERMINED") {
   constexpr matrix_<float, 3, 3> matrix{1.f, 2.f, 3.f, 4.f, 5.f,
                                         6.f, 7.f, 8.f, 9.f};
-  constexpr auto mat = lab::num_methods::gauss_elimination(matrix);
+  constexpr auto mat = kraken::num_methods::gauss_elimination(matrix);
   REQUIRE(mat.at<1, 0>() == 0.f);
   REQUIRE(mat.at<2, 0>() == 0.f);
   REQUIRE(mat.at<2, 1>() == 0.f);
-  constexpr auto deter = lab::num_methods::determined(matrix);
+  constexpr auto deter = kraken::num_methods::determined(matrix);
   REQUIRE(deter == 0.f);
 }
 
 TEST_CASE("CHECK LEAST-SQUARES") {
   constexpr matrix_<float, 1, 8> xi{0, 1, 1, 2, 3, 4, 2, 5};
   constexpr matrix_<float, 1, 8> yi{3, 4, 6, 9, 11, 15, 10, 16};
-  constexpr auto actual = lab::num_methods::least_squares(xi, yi);
+  constexpr auto actual = kraken::num_methods::least_squares(xi, yi);
   constexpr float expected = 5.75641f;
   REQUIRE(expected == actual);
 }
@@ -31,7 +31,7 @@ TEST_CASE("CRAMER'S RULE") {
   };
   constexpr matrix_<float, 1, 3> right_side{12, 11, 2};
   const auto actual =
-    std::move(lab::num_methods::cramer(mat, right_side));
+    std::move(kraken::num_methods::cramer(mat, right_side));
   constexpr matrix_<float, 1, 3> expected
   {2.99999928474f, 1.00000035763f, 1.99999976158f};
   REQUIRE(expected == actual);
@@ -39,7 +39,7 @@ TEST_CASE("CRAMER'S RULE") {
 
 TEST_CASE("SIMPSON'S RULE") {
   constexpr auto actual =
-      lab::num_methods::simpson(6, 1.f, 3.f,
+      kraken::num_methods::simpson(6, 1.f, 3.f,
                 [](auto &&i) { return i * i; });
   constexpr float expected = 8.6666669846f;
   REQUIRE(expected == actual);
@@ -47,7 +47,7 @@ TEST_CASE("SIMPSON'S RULE") {
 
 TEST_CASE("NEWTON'S METHOD") {
   constexpr auto val = 2;
-  constexpr auto actual = lab::num_methods::newton(1.f, 20.f, [&val](auto x) {
+  constexpr auto actual = kraken::num_methods::newton(1.f, 20.f, [&val](auto x) {
                                           return (x*x) - val;
                                         }, [](auto x){
                                           return (2.*x);
