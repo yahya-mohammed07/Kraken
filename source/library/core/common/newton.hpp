260859e54393dfbf14b8b3a3052ee138a30e7daa
yahya-mohammed07 <goldroger300@gmail.com>
2021-10-13 16:50:05 +0300

    `decimal point` helper functions applied

--- source/library/core/common/newton.hpp
+++ source/library/core/common/newton.hpp
@@ -28,7 +28,7 @@ SOFTWARE.
 */
 
 
-#include "float_comp.hpp"
+#include "common/comp_decimal_point_nums.hpp"    // comparing numbers with decimal point
 
 namespace kraken::num_methods {
     template<class A, class Op1, class Op2>
