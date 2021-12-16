69a7b828d40e93ca564bafacd5791737e206b478
yahya-mohammed07 <goldroger300@gmail.com>
2021-12-16 16:37:36 +0300

    symbolic constants replaced with alphabetical ones

--- source/library/core/constants.hpp
+++ source/library/core/constants.hpp
@@ -30,181 +30,227 @@ SOFTWARE.
 
 #include <concepts>
 #include <type_traits>
-
-/// @brief math and physics constants in one place
-
+/// @brief math and physics constants in one place!
 namespace kraken::constants {
 
   template <class Ty>
   concept check_float = std::is_floating_point_v<Ty>;
 
+/*
   ///@brief Pi, Archimedes' constant or Ludolph's number
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty π_v = static_cast<Ty>(3.14159'26535'89793'23846L);
+  inline constexpr check_float auto π_v = static_cast<Ty>(3.14159'26535'89793'23846L);
   ///@brief Pi, Archimedes' constant or Ludolph's number
-  inline constexpr double π = π_v<double>;
-  ///@brief 1/π
-  template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty inv_π_v = static_cast<Ty>(0.318309886183790671537767526745028724L);
-  ///@brief 1/π
-  inline constexpr double inv_π = inv_π_v<double>;
-  ///@brief 1/sqrt(π)
-  template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty inv_sqrt_π_v =
-            static_cast<Ty>(0.564189583547756286948079451560772586L);
-  ///@brief 1/sqrt(π)
-  inline constexpr double inv_sqrt_π = inv_sqrt_π_v<double>;
+  inline constexpr auto π = π_v<double>;
   ///@brief Bernstein's constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty β_v = static_cast<Ty>(0.28016'94990'23869'13303L);
+  inline constexpr check_float auto β_v = static_cast<Ty>(0.28016'94990'23869'13303L);
   ///@brief Bernstein's constant
-  inline constexpr double β = β_v<double>;
+  inline constexpr auto β = β_v<double>;
   ///@brief Embree–Trefethen constant
   inline constexpr float β_star = 0.70258f;
-  ///@brief e, Napier's constant, or Euler's number
-  template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty e_v = static_cast<Ty>(2.71828'18284'59045'23536'02874L);
-  ///@brief e, Napier's constant, or Euler's number
-  inline constexpr double e = e_v<double>;
   ///@brief Feigenbaum constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty α_v = static_cast<Ty>(2.50290'78750'95892'82228L);
+  inline constexpr check_float auto α_v = static_cast<Ty>(2.50290'78750'95892'82228L);
   ///@brief Feigenbaum constant
-  inline constexpr double α = α_v<double>;
+  inline constexpr auto α = α_v<double>;
   ///@brief Reciprocal Fibonacci constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty ψ_v = static_cast<Ty>(3.35988'56662'43177'55317'201137L);
+  inline constexpr check_float auto ψ_v = static_cast<Ty>(3.35988'56662'43177'55317'201137L);
   ///@brief Reciprocal Fibonacci constant
-  inline constexpr double ψ = ψ_v<double>;
+  inline constexpr auto ψ = ψ_v<double>;
   ///@brief Feigenbaum constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty δ_v = static_cast<Ty>(4.66920'16091'02990'67185'32038L);
+  inline constexpr check_float auto δ_v = static_cast<Ty>(4.66920'16091'02990'67185'32038L);
   ///@brief Feigenbaum constant
-  inline constexpr double δ = δ_v<double>;
+  inline constexpr auto δ = δ_v<double>;
   ///@brief omega constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty Ω_v = static_cast<Ty>(0.56714'32904'09783'87299'99686L);
+  inline constexpr check_float auto Ω_v = static_cast<Ty>(0.56714'32904'09783'87299'99686L);
   ///@brief omega constant
-  inline constexpr double Ω = Ω_v<double>;
+  inline constexpr auto Ω = Ω_v<double>;
   ///@brief Conway's constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty λ_conway_v =
+  inline constexpr check_float auto λ_conway_v =
       static_cast<Ty>(1.30357'72690'34296'39125'70991L);
   ///@brief Conway's constant
   inline constexpr double λ_conway = λ_conway_v<double>;
   ///@brief Gauss–Kuzmin–Wirsing constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty λ_gauss_v =
+  inline constexpr check_float auto λ_gauss_v =
       static_cast<Ty>(0.30366'30028'98732'65859'74481L);
   ///@brief Gauss–Kuzmin–Wirsing constant
   inline constexpr auto λ_gauss = λ_gauss_v<double>;
   ///@brief Golomb–Dickman constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty λ_golomb_v =
+  inline constexpr check_float auto λ_golomb_v =
       static_cast<Ty>(0.62432'99885'43550'87099'29363L);
   ///@brief Golomb–Dickman constant
   inline constexpr auto λ_golomb = λ_golomb_v<double>;
   ///@brief Apéry's constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty ζ_3_v = static_cast<Ty>(1.20205'69031'59594'28539'97381L);
+  inline constexpr check_float auto ζ_3_v = static_cast<Ty>(1.20205'69031'59594'28539'97381L);
   ///@brief Apéry's constant
   inline constexpr auto ζ_3 = ζ_3_v<double>;
   ///@brief Euler–Mascheroni constant
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty γ_v = static_cast<Ty>(0.57721'56649'01532'86060L);
+  inline constexpr check_float auto γ_v = static_cast<Ty>(0.57721'56649'01532'86060L);
   ///@brief Euler–Mascheroni constant
   inline constexpr auto γ = γ_v<double>;
   ///@brief Golden ratio
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty ϕ_v = static_cast<Ty>(1.61803'39887'49894'84820'45868L);
+  inline constexpr check_float auto ϕ_v = static_cast<Ty>(1.61803'39887'49894'84820'45868L);
   ///@brief Golden ratio
-  inline constexpr double ϕ = ϕ_v<double>;
+  inline constexpr auto ϕ = ϕ_v<double>;
+  ///@brief Magnetic permeability of a vacuum in `N*s2/C2`
+  template <class Ty>
+  inline constexpr check_float auto μ0_v = static_cast<Ty>(4 * 3.14159'26535'89793'23846e-7L);
+  ///@brief Magnetic permeability of a vacuum in `N*s2/C2`
+  inline constexpr auto μ0 = μ0_v<double>;
+*/
+
+  ///@brief Pi, Archimedes' constant or Ludolph's number π
+  template <class Ty>
+  inline constexpr check_float auto pi_v {static_cast<Ty>(3.14159'26535'89793'23846)};
+  ///@brief Pi, Archimedes' constant or Ludolph's number
+  inline constexpr auto pi = pi_v<double>;
+  ///@brief 1/π
+  template <class Ty>
+  inline constexpr check_float auto inv_pi_v {static_cast<Ty>(0.318309886183790671537767526745028724)};
+  ///@brief 1/π
+  inline constexpr auto inv_pi {inv_pi_v<double>};
+  ///@brief 1/sqrt(π)
+  template <class Ty>
+  inline constexpr check_float auto inv_sqrt_pi_v
+            {static_cast<Ty>(0.564189583547756286948079451560772586)};
+  ///@brief 1/sqrt(π)
+  inline constexpr auto inv_sqrt_pi {inv_sqrt_pi_v<double>};
+  ///@brief Bernstein's constant
+  template <class Ty>
+  inline constexpr check_float auto beta_v {static_cast<Ty>(0.28016'94990'23869'13303)};
+  ///@brief Bernstein's constant β
+  inline constexpr auto B {beta_v<double>};
+  ///@brief Embree–Trefethen constant β*
+  inline constexpr float B_star {0.70258f};
+  ///@brief e, Napier's constant, or Euler's number
+  template <class Ty>
+  inline constexpr check_float auto e_v {static_cast<Ty>(2.71828'18284'59045'23536'02874)};
+  ///@brief e, Napier's constant, or Euler's number
+  inline constexpr auto e {e_v<double>};
+  ///@brief Feigenbaum constant α
+  template <class Ty>
+  inline constexpr check_float auto alpha_v {static_cast<Ty>(2.50290'78750'95892'82228)};
+  ///@brief Feigenbaum constant α
+  inline constexpr auto alpha {alpha_v<double>};
+  ///@brief Reciprocal Fibonacci constant ψ
+  template <class Ty>
+  inline constexpr check_float auto sai_v {static_cast<Ty>(3.35988'56662'43177'55317'201137)};
+  ///@brief Reciprocal Fibonacci constant ψ
+  inline constexpr auto sai {sai_v<double>};
+  ///@brief Feigenbaum constant δ
+  template <class Ty>
+  inline constexpr check_float auto delta_v {static_cast<Ty>(4.66920'16091'02990'67185'32038)};
+  ///@brief Feigenbaum constant δ
+  inline constexpr auto delta {delta_v<double>};
+  ///@brief omega constant Ω
+  template <class Ty>
+  inline constexpr check_float auto omega_v {static_cast<Ty>(0.56714'32904'09783'87299'99686)};
+  ///@brief omega constant Ω
+  inline constexpr auto omega {omega_v<double>};
+  ///@brief Conway's constant
+  template <class Ty>
+  inline constexpr check_float auto lambda_conway_v
+      {static_cast<Ty>(1.30357'72690'34296'39125'70991)};
+  ///@brief Conway's constant
+  inline constexpr auto lambda_conway {lambda_conway_v<double>};
+  ///@brief Gauss–Kuzmin–Wirsing constant
+  template <class Ty>
+  inline constexpr check_float auto lambda_gauss_v
+      {static_cast<Ty>(0.30366'30028'98732'65859'74481)};
+  ///@brief Gauss–Kuzmin–Wirsing constant
+  inline constexpr auto lambda_gauss {lambda_gauss_v<double>};
+  ///@brief Golomb–Dickman constant
+  template <class Ty>
+  inline constexpr check_float auto lambda_golomb_v
+      {static_cast<Ty>(0.62432'99885'43550'87099'29363)};
+  ///@brief Golomb–Dickman constant
+  inline constexpr auto lambda_golomb {lambda_golomb_v<double>};
+  ///@brief Apéry's constant ζ_3
+  template <class Ty>
+  inline constexpr check_float auto zeta_3_v {static_cast<Ty>(1.20205'69031'59594'28539'97381)};
+  ///@brief Apéry's constant ζ_3
+  inline constexpr auto zeta_3 {zeta_3_v<double>};
+  ///@brief Euler–Mascheroni constant γ
+  template <class Ty>
+  inline constexpr check_float auto gamma_v {static_cast<Ty>(0.57721'56649'01532'86060)};
+  ///@brief Euler–Mascheroni constant γ
+  inline constexpr auto gamma {gamma_v<double>};
+  ///@brief Golden ratio ϕ
+  template <class Ty>
+  inline constexpr check_float auto phi_v {static_cast<Ty>(1.61803'39887'49894'84820'45868)};
+  ///@brief Golden ratio ϕ
+  inline constexpr auto phi {phi_v<double>};
   ///@brief Elementary charge in `C`
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty e_charge_v = static_cast<Ty>(1.602176634e-19L);
+  inline constexpr check_float auto e_charge_v {static_cast<Ty>(1.602176634e-19)};
   ///@brief Elementary charge in `C`
-  inline constexpr double e_charge = e_charge_v<double>;
+  inline constexpr auto e_charge {e_charge_v<double>};
   ///@brief Planck constant in `J*Hz`
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty h_v = static_cast<Ty>(6.62607015e-34L);
+  inline constexpr check_float auto h_v {static_cast<Ty>(6.62607015e-34)};
   ///@brief Planck constant in `J*Hz`
-  inline constexpr double h = h_v<double>;
+  inline constexpr auto h {h_v<double>};
   ///@brief speed of light in `m/s`
-  inline constexpr std::size_t c = 299792458;
+  inline constexpr std::size_t c {299792458ull};
   ///@brief Gravity of earth in `m/s^2`
-  inline constexpr float g = 9.80665f;
+  inline constexpr float g {9.80665f};
   ///@brief Newtonian constant of gravitation in `m^3⋅kg^-1`
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty G_v = static_cast<Ty>(6.67430 * 15e-11L);
+  inline constexpr check_float auto G_v {static_cast<Ty>(6.67430 * 15e-11)};
   ///@brief Newtonian constant of gravitation in `m^3⋅kg^-1`
-  inline constexpr auto G = G_v<double>;
-  ///@brief Magnetic permeability of a vacuum in `N*s2/C2`
+  inline constexpr auto G {G_v<double>};
+  ///@brief Magnetic permeability of a vacuum in `N*s2/C2` μ0
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty μ0_v = static_cast<Ty>(4 * 3.14159'26535'89793'23846e-7L);
-  ///@brief Magnetic permeability of a vacuum in `N*s2/C2`
-  inline constexpr double μ0 = μ0_v<double>;
+  inline constexpr check_float auto mu0_v {static_cast<Ty>(4 * 3.14159'26535'89793'23846e-7)};
+  ///@brief Magnetic permeability of a vacuum in `N*s2/C2` μ0
+  inline constexpr auto mu0 {mu0_v<double>};
   ///@brief log_2e
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty log2e_v =
-      static_cast<Ty>(1.442695040888963407359924681001892137L);
+  inline constexpr check_float auto log2e_v
+      {static_cast<Ty>(1.442695040888963407359924681001892137)};
   ///@brief log_2e
-  inline constexpr double log2e = log2e_v<double>;
+  inline constexpr auto log2e {log2e_v<double>};
   ///@brief log_10e
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty log10e_v = static_cast<Ty> (0.434294481903251827651128918916605082L);
+  inline constexpr check_float auto log10e_v {static_cast<Ty> (0.434294481903251827651128918916605082)};
   ///@brief log_10e
-  inline constexpr double log10e = log2e_v<double>;
+  inline constexpr auto log10e {log2e_v<double>};
   ///@brief log_e 2
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty ln2_v = static_cast<Ty> (0.693147180559945309417232121458176568L);
+  inline constexpr check_float auto ln2_v {static_cast<Ty> (0.693147180559945309417232121458176568)};
   ///@brief log_e 2
-  inline constexpr double ln2 = ln2_v<double>;
+  inline constexpr auto ln2 = ln2_v<double>;
   ///@brief log_e 10
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty ln10_v = static_cast<Ty> (2.302585092994045684017991454684364208L);
+  inline constexpr check_float auto ln10_v {static_cast<Ty> (2.302585092994045684017991454684364208)};
   ///@brief log_e 10
-  inline constexpr double ln10 = ln10_v<double>;
+  inline constexpr auto ln10 {ln10_v<double>};
   ///@brief sqrt(2)
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty sqrt2_v = static_cast<Ty> (1.414213562373095048801688724209698079L);
+  inline constexpr check_float auto sqrt2_v {static_cast<Ty> (1.414213562373095048801688724209698079)};
   ///@brief sqrt(2)
-  inline constexpr double sqrt2 = sqrt2_v<double>;
+  inline constexpr auto sqrt2 {sqrt2_v<double>};
   ///@brief sqrt(3)
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty sqrt3_v = static_cast<Ty> (1.732050807568877293527446341505872367L);
+  inline constexpr check_float auto sqrt3_v {static_cast<Ty> (1.732050807568877293527446341505872367)};
   ///@brief sqrt(3)
-  inline constexpr double sqrt3 = sqrt3_v<double>;
+  inline constexpr auto sqrt3 {sqrt3_v<double>};
   ///@brief 1/sqrt(3)
   template <class Ty>
-  requires check_float<Ty>
-  inline constexpr Ty inv_sqrt3_v = static_cast<Ty> (0.577350269189625764509148780501957456L);
+  inline constexpr check_float auto inv_sqrt3_v {static_cast<Ty> (0.577350269189625764509148780501957456)};
   ///@brief 1/sqrt(3)
-  inline constexpr double inv_sqrt3 = inv_sqrt3_v<double>;
+  inline constexpr auto inv_sqrt3 {inv_sqrt3_v<double>};
+
 } // namespace kraken::constants
 
 #endif // CONSTANTS_HPP
