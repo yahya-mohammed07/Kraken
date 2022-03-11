77113da782f01b53483569893d18f712dfb39442
yahya-mohammed07 <goldroger300@gmail.com>
2022-03-11 19:44:47 +0300

    matrix multiplication improved and more

--- source/library/core/matrix.hpp
+++ source/library/core/matrix.hpp
@@ -27,11 +27,10 @@ SOFTWARE.
 
 */
 
-
+#include <iomanip>            // std::setw
 #include <algorithm>          // std::swap
 #include <array>              // std::array
 #include <initializer_list>
-#include <cstdio>             // std::puts, std::printf
 #include <cassert>            // assert
 #include <type_traits>
 #include <ostream>            // std::ostream
@@ -44,14 +43,25 @@ struct row_col
   std::size_t col{};
 };
 
-template <class T, std::size_t ROW, std::size_t COL>
-  requires (!std::is_class_v<T>)
+template <class Ty, std::size_t ROW, std::size_t COL>
+  requires (!std::is_class_v<Ty>)
 class matrix_
 {
+public:
+  using value_type              = Ty;
+  using pointer                 = value_type*;
+  using const_pointer           = const value_type*;
+  using reference               = value_type&;
+  using const_reference         = const value_type&;
+  using iterator                =  value_type*;
+  using const_iterator          = const value_type*;
+  using size_type               = std::size_t;
+  using reverse_iterator        = std::reverse_iterator<iterator>;
+  using const_reverse_iterator  = std::reverse_iterator<const_iterator>;
   static_assert(ROW > 0UL && COL > 0UL, "ROW-COL must be greater than `0`");
 
 private:
-  std::array<T, (ROW*COL)>  m_data{};
+  std::array<value_type, (ROW*COL)>  m_data{};
 
 public:
 
@@ -72,56 +82,62 @@ public:
   }
 
   /// @brief init-list constructor
-  constexpr matrix_(const std::initializer_list<T> &init) noexcept
+  constexpr matrix_(const std::initializer_list<value_type> &init) noexcept
   {
-    for (std::size_t j {0} ; const auto &i : init) {
+    for (size_type j {0} ; const auto &i : init) {
       m_data.at(j) = std::move(i);
       ++i;
     }
   }
 
   /// @brief init-list constructor
-  constexpr matrix_(std::initializer_list<T> &&init) noexcept
+  constexpr matrix_(std::initializer_list<value_type> &&init) noexcept
   {
-    for (std::size_t j {0} ; auto &&i : init) {
+    for (size_type j {0} ; auto &&i : init) {
       m_data.at(j) = i;
       ++j;
     }
   }
 
   /// @brief variadic constructor
+  template< class ...T >
   explicit
-  consteval matrix_( auto ...args )
+  consteval matrix_( const T ...args )
   {
-    m_data = {std::move(args)...};
+    if constexpr ( sizeof...(T) == 1) {
+      fill( std::forward<value_type>(args)... );
+    }
+    else {
+      m_data = {std::move(args)...};
+    }
   }
 
   constexpr ~matrix_() = default;
 
   ///
   [[nodiscard]] constexpr
-  auto begin() const noexcept     { return m_data.begin(); }
+  const_iterator begin() const noexcept             { return m_data.begin(); }
   [[nodiscard]] constexpr
-  auto end() const noexcept       { return m_data.end();   }
+  const_iterator end() const noexcept               { return m_data.end();   }
   [[nodiscard]] constexpr
-  auto rbegin() const noexcept    { return m_data.rbegin(); }
+  const_reverse_iterator rbegin() const noexcept    { return m_data.rbegin(); }
   [[nodiscard]] constexpr
-  auto rend() const noexcept      { return m_data.rend();   }
+  const_reverse_iterator rend() const noexcept      { return m_data.rend();   }
   ///
   [[nodiscard]] constexpr
-  auto begin() noexcept           { return m_data.begin(); }
+  iterator begin() noexcept                         { return m_data.begin(); }
   [[nodiscard]] constexpr
-  auto end() noexcept             { return m_data.end();   }
+  iterator end() noexcept                           { return m_data.end();   }
   [[nodiscard]] constexpr
-  auto rbegin() noexcept          { return m_data.rbegin(); }
+  reverse_iterator rbegin() noexcept                { return m_data.rbegin(); }
   [[nodiscard]] constexpr
-  auto rend() noexcept            { return m_data.rend();   }
+  reverse_iterator rend() noexcept                  { return m_data.rend();   }
   /// @methods:
 
   /// @get: column
   [[nodiscard]] constexpr
   auto col() const
-      -> std::size_t
+      -> size_type
   {
     return COL;
   }
@@ -129,7 +145,7 @@ public:
   /// @get: row
   [[nodiscard]] constexpr
   auto row() const
-      -> std::size_t
+      -> size_type
   {
     return ROW;
   }
@@ -144,7 +160,7 @@ public:
 
   [[nodiscard]] constexpr
   auto size() const
-    -> std::size_t
+    -> size_type
   {
     return ROW*COL;
   }
@@ -161,11 +177,11 @@ public:
   /// @brief get/modify element at given row-col
   /// @tparam row row-number
   /// @tparam col column-number
-  /// @return T&
-  template<const std::size_t row, std::size_t col>
+  /// @return reference
+  template<const size_type row, size_type col>
   [[nodiscard]] constexpr
   auto at()
-      -> auto &
+      -> reference
   {
     static_assert( (row >= 0 && col >= 0) && (row < ROW && col < COL) );
     return m_data[(row * COL) + col];
@@ -174,11 +190,11 @@ public:
   /// @brief get element at given row-col
   /// @tparam row row-number
   /// @tparam col column-number
-  /// @return T&
-  template<const std::size_t row, std::size_t col>
+  /// @return value_type
+  template<const size_type row, size_type col>
   [[nodiscard]] constexpr
   auto at() const
-      -> auto
+      -> value_type
   {
     static_assert( (row >= 0 && col >= 0) && (row < ROW && col < COL) );
     return m_data[(row * COL) + col];
@@ -187,24 +203,22 @@ public:
   /// @brief get/modify element at given row-col
   /// @param row row-number
   /// @param col column-number
-  /// @return T&
+  /// @return reference
   [[nodiscard]] constexpr
-  auto at(const std::size_t &row, const std::size_t &col)
-      -> auto &
+  auto at(const size_type &row, const size_type &col)
+      -> reference
   {
-    assert( (row >= 0 && col >= 0) && (row < ROW && col < COL) );
     return m_data[(row * COL) + col];
   }
 
   /// @brief get element at given row-col
   /// @param row row-number
   /// @param col column-number
-  /// @return T
+  /// @return value_type
   [[nodiscard]] constexpr
-  auto at(const std::size_t &row, const std::size_t &col) const
-      -> auto
+  auto at(const size_type &row, const size_type &col) const
+      -> value_type
   {
-    assert( (row >= 0 && col >= 0) && (row < ROW && col < COL) );
     return m_data[(row * COL) + col];
   }
 
@@ -212,10 +226,10 @@ public:
   /// @param start which row
   /// @param with change with row
   constexpr
-  auto swap_rows(const std::size_t &start, const std::size_t &with)
+  auto swap_rows(const size_type &start, const size_type &with)
       -> void
   {
-    for (std::size_t i{0}; i < ROW; ++i) {
+    for (size_type i{0}; i < ROW; ++i) {
       std::swap(at(start, i), at(with, i));
     }
   }
@@ -224,10 +238,10 @@ public:
   /// @param start which column
   /// @param with change with column
   constexpr
-  auto swap_cols(const std::size_t &start, const std::size_t &with)
+  auto swap_cols(const size_type &start, const size_type &with)
       -> void
   {
-    for (std::size_t i{0}; i < COL; ++i) {
+    for (size_type i{0}; i < COL; ++i) {
       std::swap(at(i, start), at(i, with));
     }
   }
@@ -242,8 +256,8 @@ public:
   {
     if ( size() < 256UL ) { // insertion-sort
       if ( order ) {
-        for ( std::size_t i {0}; i < size(); ++i) {
-          std::size_t j {i};
+        for ( size_type i {0}; i < size(); ++i) {
+          size_type j {i};
           while ( j != 0 && m_data[j-1] > m_data[j] ) {
             std::swap(m_data[j-1] , m_data[j]);
             --j;
@@ -251,8 +265,8 @@ public:
         }
         return;
       }
-      for ( std::size_t i {0}; i < size(); ++i) {
-        std::size_t j {i};
+      for ( size_type i {0}; i < size(); ++i) {
+        size_type j {i};
         while ( j != 0 && m_data[j-1] < m_data[j] ) {
           std::swap(m_data[j-1] , m_data[j]);
           --j;
@@ -261,7 +275,7 @@ public:
       return;
     }
     if ( order ) { std::sort( begin(), end() ); return; }
-    std::sort( begin(), end(), std::greater<T>{} );
+    std::sort( begin(), end(), std::greater<value_type>{} );
   }
 
   /// @brief changes its rows into columns and its columns into rows
@@ -269,8 +283,8 @@ public:
   constexpr
   auto transpose_squared() -> void
   {
-    for (std::size_t i {0}; i < ROW-1; ++i) {
-      for (std::size_t j {i+1}; j < COL; ++j) {
+    for (size_type i {0}; i < ROW-1; ++i) {
+      for (size_type j {i+1}; j < COL; ++j) {
         std::swap( at(i,j), at(j,i) );
       }
     }
@@ -281,9 +295,9 @@ public:
   [[nodiscard]] constexpr
   auto transpose_triangular() const
   {
-    matrix_<T, COL, ROW> temp{};
-    for ( std::size_t i {}; i < ROW; ++i ) {
-      for ( std::size_t j {}; j < COL; ++j ) {
+    matrix_<value_type, COL, ROW> temp{};
+    for ( size_type i {}; i < ROW; ++i ) {
+      for ( size_type j {}; j < COL; ++j ) {
         temp[(ROW*j)+i] = std::move(m_data[(COL*i)+j]);
       }
     }
@@ -293,7 +307,7 @@ public:
   /// @brief fills the container with a certain value
   /// @return nothing
   constexpr
-  auto fill( T&& value ) -> void
+  auto fill( value_type&& value ) -> void
   {
     for ( auto &&i : m_data ) {
       i = std::move(value);
@@ -306,16 +320,16 @@ public:
   /// @tparam col - new column after transposing
   /// @tparam clock_wise - default = true
   /// @return nothing
-  template<const std::size_t row, const std::size_t col
+  template<const size_type row, const size_type col
                       ,const bool clock_wise = true>
   constexpr
   auto rotate_triangular()
     -> void
   {
     if constexpr ( clock_wise ) {
-      for ( std::size_t i {0}; i < col; ++i ) {
-        std::size_t start {0};
-        std::size_t end   {row-1};
+      for ( size_type i {0}; i < col; ++i ) {
+        size_type start {0};
+        size_type end   {row-1};
         while ( start < end ) {
           std::swap( at(start, i), at(end, i) );
           ++start;
@@ -324,9 +338,9 @@ public:
       }
       return;
     }
-    for ( std::size_t i {0}; i < row; ++i ) {
-      std::size_t start {0};
-      std::size_t end   {col-1};
+    for ( size_type i {0}; i < row; ++i ) {
+      size_type start {0};
+      size_type end   {col-1};
       while ( start < end ) {
         std::swap( at(i, start), at(i, end) );
         ++start;
@@ -345,20 +359,20 @@ public:
     -> void
   {
     if constexpr ( clock_wise ) {
-      std::size_t k {0};
-      for ( std::size_t i {0}; i < ROW; ++i ) {
+      size_type k {0};
+      for ( size_type i {0}; i < ROW; ++i ) {
         k = ROW-1;
-        for ( std::size_t j {0}; j < k; ++j ) {
+        for ( size_type j {0}; j < k; ++j ) {
           std::swap( at(i, j), at(i, k) );
           --k;
         }
       }
       return;
     }
-    std::size_t k {0};
-    for ( std::size_t i {0}; i < ROW; ++i ) {
+    size_type k {0};
+    for ( size_type i {0}; i < ROW; ++i ) {
       k = ROW-1;
-      for ( std::size_t j {0}; j < k; ++j ) {
+      for ( size_type j {0}; j < k; ++j ) {
         std::swap( at(j, i), at(k, i) );
         --k;
       }
@@ -389,7 +403,7 @@ public:
 
   constexpr matrix_ &operator+=(const matrix_ &rhs) noexcept
   {
-    for ( std::size_t j{} ; const auto& i : rhs ) {
+    for ( size_type j{} ; const auto& i : rhs ) {
       m_data[j] += i;
       ++j;
     }
@@ -404,7 +418,7 @@ public:
     return lhs;
   }
 
-  constexpr matrix_ &operator+=(const T scalar) noexcept
+  constexpr matrix_ &operator+=(const value_type scalar) noexcept
   {
     for ( auto && i : *this ) { i += scalar; }
     return *this;
@@ -412,7 +426,7 @@ public:
 
   /// @brief adds a matrix containers with a scalar
   /// @return matrix
-  constexpr friend matrix_ operator+(matrix_ lhs , const T scalar) noexcept
+  constexpr friend matrix_ operator+(matrix_ lhs , const value_type scalar) noexcept
   {
     lhs += scalar;
     return lhs;
@@ -421,7 +435,7 @@ public:
   /// @brief multiplies a matrix containers with a `scalar`
   /// @return matrix
   [[nodiscard]] constexpr
-  matrix_& operator*( T val)  noexcept
+  matrix_& operator*( value_type val)  noexcept
   {
     for ( auto &&i : *this ) {
       i *= val;
@@ -436,48 +450,16 @@ public:
   matrix_ operator*(const matrix_ &rhs)  noexcept
   {
     if constexpr ( ROW == 1UL ) {
-      for ( std::size_t j{} ; const auto& i : rhs ) {
+      for ( size_type j{} ; const auto& i : rhs ) {
         m_data[j] *= i;
         ++j;
       }
       return *this;
     }
-    matrix_<T , ROW, COL> temp{};
-    for (std::size_t i {0}; i < ROW; i++)
-    {
-      for (std::size_t j {0}; j < rhs.col(); j++)
-      {
-        temp.at(i,j) = 0;
-        for (std::size_t k {0}; k < rhs.row(); k++)
-        {
-          temp.at(i,j) += (at(i,k) * rhs.at(k,j));
-        }
-      }
-    }
-    return temp;
-  }
-
-  /// @brief multiplies two matrix collections
-  /// `col` of a must be equal to `row` b
-  /// @return matrix
-  [[nodiscard]] constexpr
-  matrix_ operator*(matrix_ &&rhs) noexcept
-  {
-    if constexpr ( ROW == 1UL ) {
-      for ( std::size_t j{} ; const auto& i : rhs ) {
-        m_data[j] *= i;
-        ++j;
-      }
-      return *this;
-    }
-    matrix_<T , ROW, COL> temp{};
-    for (std::size_t i {0}; i < ROW; i++)
-    {
-      for (std::size_t j {0}; j < rhs.col(); j++)
-      {
-        temp.at(i,j) = 0;
-        for (std::size_t k {0}; k < rhs.row(); k++)
-        {
+    matrix_<value_type , ROW, COL> temp{};
+   for (size_type i {0}; i < ROW; ++i) {
+      for (size_type k {0}; k < rhs.row(); ++k) {
+        for (size_type j {0}; j < COL; ++j) {
           temp.at(i,j) += (at(i,k) * rhs.at(k,j));
         }
       }
@@ -488,40 +470,14 @@ public:
   /// @brief multiplies two matrix containers
   /// `col` of a must be equal to `row` b
   /// @return matrix
-  template<const std::size_t L>
+  template<const size_type L>
   [[nodiscard]] constexpr
-  matrix_<T , ROW, L> operator*(const matrix_<T , COL, L> &rhs) const noexcept
+  matrix_<value_type , ROW, L> operator*(const matrix_<value_type , COL, L> &rhs) const noexcept
   {
-    matrix_<T , ROW, L> temp{};
-    for (std::size_t i {0}; i < ROW; i++)
-    {
-      for (std::size_t j {0}; j < rhs.col(); j++)
-      {
-        temp.at(i,j) = 0;
-        for (std::size_t k {0}; k < rhs.row(); k++)
-        {
-          temp.at(i,j) += (at(i,k) * rhs.at(k,j));
-        }
-      }
-    }
-    return temp;
-  }
-
-  /// @brief multiplies two matrix collections
-  /// col` of a must be equal to `row` b
-  /// @return matrix
-  template<const std::size_t L>
-  [[nodiscard]] constexpr
-  matrix_<T , ROW, L> operator*(matrix_<T , COL, L> &&rhs) const noexcept
-  {
-    matrix_<T , ROW, L> temp{};
-    for (std::size_t i {0}; i < row(); i++)
-    {
-      for (std::size_t j {0}; j < rhs.col(); j++)
-      {
-        temp.at(i,j) = 0;
-        for (std::size_t k {0}; k < rhs.row(); k++)
-        {
+    matrix_<value_type , ROW, L> temp{};
+   for (size_type i {0}; i < ROW; ++i) {
+      for (size_type k {0}; k < rhs.row(); ++k) {
+        for (size_type j {0}; j < L; ++j) {
           temp.at(i,j) += (at(i,k) * rhs.at(k,j));
         }
       }
@@ -531,7 +487,7 @@ public:
 
   constexpr matrix_ &operator-=(const matrix_ &rhs) noexcept
   {
-    for ( std::size_t j{} ; const auto& i : rhs ) {
+    for ( size_type j{} ; const auto& i : rhs ) {
       m_data[j] -= i;
       ++j;
     }
@@ -546,7 +502,7 @@ public:
     return lhs;
   }
 
-  constexpr matrix_ &operator-=(const T scalar) noexcept
+  constexpr matrix_ &operator-=(const value_type scalar) noexcept
   {
     for ( auto && i : *this ) { i -= scalar; }
     return *this;
@@ -554,37 +510,26 @@ public:
 
   /// @brief adds a matrix containers with a scalar
   /// @return matrix
-  constexpr friend matrix_ operator-(matrix_ lhs , const T scalar) noexcept
+  constexpr friend matrix_ operator-(matrix_ lhs , const value_type scalar) noexcept
   {
     lhs -= scalar;
     return lhs;
   }
 
-  /// @brief subtructs a matrix containers with a scalar
-  /// @return matrix
-  constexpr matrix_& operator-(const T val) noexcept
-  {
-    for ( std::size_t j{} ; auto &&i : *this ) {
-      m_data[j] -= val;
-      ++j;
-    }
-    return *this;
-  }
-
   /// @brief get/modify an element in a given index
   /// @brief Subscript access to the data contained in the %matrix
-  /// @param i The index of the element for which data should be accessed.
-  /// @return T&
-  constexpr T& operator[](const std::size_t i) noexcept
+  /// @param i index of the element for which data should be accessed.
+  /// @return value_type&
+  constexpr value_type& operator[](const size_type i) noexcept
   {
     return m_data[i];
   }
 
   /// @brief get an element in a given index
   /// @brief Subscript access to the data contained in the %matrix
-  /// @param i The index of the element for which data should be accessed.
-  /// @return T
-  constexpr T operator[](const std::size_t i) const noexcept
+  /// @param i index of the element for which data should be accessed.
+  /// @return value_type
+  constexpr value_type operator[](const size_type i) const noexcept
   {
     return m_data[i];
   }
@@ -602,10 +547,10 @@ public:
   constexpr
   friend std::ostream &operator<<(std::ostream &os, const matrix_ & mat)
   {
-    for (std::size_t count{}; auto &&i : mat) {
-      if (count == COL) { std::printf("%c", '\n'); count=0; }
-      if constexpr ( std::is_floating_point_v<T> ) {
-        std::printf("%.11f ", i);
+    for (size_type count{}; auto &&i : mat) {
+      if (count % COL == 0) { os << '\n'; count=0; }
+      if constexpr ( std::is_floating_point_v<value_type> ) {
+        os << std::setw(7);
       } else {
         os << i << ' ';
       }
