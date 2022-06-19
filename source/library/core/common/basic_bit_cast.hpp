3075abbcf504692bebc7a846be2c4c1767cdec36
yahya-mohammed07 <goldroger300@gmail.com>
2022-06-19 20:40:35 +0300

    `clangd` format applied

--- source/library/core/common/basic_bit_cast.hpp
+++ source/library/core/common/basic_bit_cast.hpp
@@ -6,18 +6,14 @@
 #include <utility>
 
 namespace kraken::cal {
-  /// @link https://en.cppreference.com/w/cpp/numeric/bit_cast
-  template <class To, class From>
-  requires (sizeof(To) == sizeof(From)) &&
-      std::is_trivially_copyable_v<From> &&
-      std::is_trivially_copyable_v<To>
-  constexpr
-  auto bit_cast(const From& src) noexcept
-    -> To
-  {
-    To dst{};
-    std::memcpy( &dst, &src, sizeof(To) );
-    return std::move(dst);
-  }
+/// @link https://en.cppreference.com/w/cpp/numeric/bit_cast
+template <class To, class From>
+requires(sizeof(To) == sizeof(From)) &&
+    std::is_trivially_copyable_v<From> &&std::is_trivially_copyable_v<
+        To> constexpr auto bit_cast(const From &src) noexcept -> To {
+  To dst{};
+  std::memcpy(&dst, &src, sizeof(To));
+  return std::move(dst);
 }
+} // namespace kraken::cal
 #endif // BASIC_BIT_CAST_HPP
